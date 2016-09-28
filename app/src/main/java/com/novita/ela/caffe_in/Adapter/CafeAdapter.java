82b60022830f6e0285f31a548d90b32a4597f18b
elanovita <elanovita.py@gmail.com>
2016-09-28 11:54:57 +0800

    create project

---
+++ app/src/main/java/com/novita/ela/caffe_in/Adapter/CafeAdapter.java
@@ -0,0 +1,11 @@
+package com.novita.ela.caffe_in.Adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by e on 28/09/16.
+ */
+
+public class CafeAdapter extends RecyclerView.Adapter<CafeAdapter.ViewHolder> {
+    
+}
